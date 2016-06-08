98a5bafa1950765c2903ea367b002119381bd101
Jannick Johnsen <jannickboese@msn.com>
2016-06-09 00:19:37 +0200

    wip

--- src/Freckle/Event.fs
+++ src/Freckle/Event.fs
@@ -7,6 +7,8 @@ type Time =
     | Time of int64
 type Freck<'e> = Freck of LazyList<Time * 'e>
 
+type Statemachine<'s,'e> = Statemachine of ('s -> ('s * Freck<'e>))
+
 module Fvent =
     
     let time = fst
@@ -16,52 +18,47 @@ module Fvent =
 module Freck =  
     open LazyList
 
-    let rec private merge l1 l2 : LazyList<Time *'e> =
-        match (l1, l2) with
-        | Nil, vs -> vs
-        | us, Nil -> us
-        | LazyList.Cons((ta,a), ps), LazyList.Cons((tb,_), _) when ta >= tb ->           
-            let mergePs () = merge ps l2
-            LazyList.consDelayed (ta,a) mergePs
-        | _, LazyList.Cons((tb,b), qs) ->           
-            let mergeQs () = merge l1 qs
-            LazyList.consDelayed (tb,b) mergeQs
-
     let pure' (a : 'a) : Freck<'a> = 
         Freck <| LazyList.ofList [(Indeterminable, a)]
 
     let map (f : 'a -> 'b) (Freck fr : Freck<'a>) : Freck<'b> = 
         fr |> LazyList.map (Fvent.map f) |> Freck
 
-    let filter (f : 'a -> bool) (fr : Freck<'a>) : Freck<'a> = undefined
-    
+    let filter (f : 'a -> bool) (Freck l : Freck<'a>) : Freck<'a> =
+        let rec inner l =
+            match l with
+            | LazyList.Cons((t,a), rest) when f a -> LazyList.consDelayed (t,a) (fun () -> inner rest)
+            | LazyList.Cons(_, rest) -> inner rest
+            | Nil -> LazyList.empty
+        Freck (inner l)
+
     let choose (f : 'a -> 'b option): Freck<'a> -> Freck<'b> = 
-        map f >> filter ((<>) None) >> map Option.get
+        map f >> filter (Option.isSome) >> map Option.get
     
-
-    let timed (fr : Freck<'a>) : Freck<Time * 'a> = undefined 
+    let timed (Freck fr : Freck<'a>) : Freck<Time * 'a> = 
+        Freck <| LazyList.map (fun (t,a) -> (t,(t,a))) fr
     
-    let dateTimed (Freck fr : Freck<'a>) : Freck<DateTime * 'a> = undefined
-
     let join (Freck fr : Freck<Freck<'a>>) : Freck<'a> = 
        LazyList.map (fun (t, Freck cfr) -> LazyList.map (tuple t) (LazyList.map snd cfr)) fr
        |> LazyList.concat
        |> Freck
+            
+    let partition (f : 'a -> bool) (fr : Freck<'a>) : (Freck<'a> * Freck<'a>) = (filter f fr, filter (not << f) fr)
     
     let bind (f : 'a -> Freck<'b>) : Freck<'a> -> Freck<'b> =
         join << (map f) 
-
-            
-    let partition (f : 'a -> bool) (fr : Freck<'a>) : (Freck<'a> * Freck<'a>) = (filter f fr, filter (not << f) fr)
-
                 
-    let listenTo<'a> (fr : Freck<obj>) : Freck<'a> = undefined
+    let listenTo<'a> (fr : Freck<obj>) : Freck<'a> = choose safeUnbox fr
 
     let combine (frA : Freck<'a>) (frB : Freck<'a>) : Freck<'a> = undefined
+    
+    let until (f : 'a -> bool) 
 
-    let mapAccumNow (f : 's -> 'a -> ('s * 'b)) (s : 's) (fr : Freck<'a>) : ('s * Freck<'b>) = 
-        undefined
+module Statemachine =
 
+    let transition (f : 's -> 'a -> ('s * 'b)) (fr : Freck<'a>) : Statemachine<'s,'b> = 
+        let t =
+ 
     let inline mapAccumNow' (f : 's -> 'a -> 's) (s : 's) (fr : Freck<'a>) : (Freck<'s>) = 
         snd <| mapAccumNow (fun s a -> let s' = f s a in (s', s')) s fr
 
